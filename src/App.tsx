2125b8f8b64e224ef0b95d561194ac75503f9015
Akara5729 <nenmanamikaze@gmail.com>
2026-07-25 09:58:59 +0700

    feat: Menambahkan Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Analytics } from '@vercel/analytics/react';
 import AppLayout from './components/layout/AppLayout';
 import ScannerView from './views/ScannerView';
 import CatalogView from './views/CatalogView';
@@ -9,12 +10,15 @@ function App() {
   const [activeTab, setActiveTab] = useState<Tab>('scanner');
 
   return (
-    <AppLayout activeTab={activeTab} onChangeTab={setActiveTab}>
-      <div className="h-full w-full">
-        {activeTab === 'scanner' && <ScannerView />}
-        {activeTab === 'catalog' && <CatalogView />}
-      </div>
-    </AppLayout>
+    <>
+      <AppLayout activeTab={activeTab} onChangeTab={setActiveTab}>
+        <div className="h-full w-full">
+          {activeTab === 'scanner' && <ScannerView />}
+          {activeTab === 'catalog' && <CatalogView />}
+        </div>
+      </AppLayout>
+      <Analytics />
+    </>
   );
 }
 
